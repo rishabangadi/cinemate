a37e4f49278cb516a5ff715068fb29817a526a20
Rishab Angadi <rishab@Rishabs-MacBook-Air.local>
2024-01-27 13:05:47 +0530

    Updated watched token

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ export default function App() {
           setError("");
 
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
             { signal: controller.signal }
           );
 
